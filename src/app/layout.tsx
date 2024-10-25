4f052c3501a646436644f96b513307b4015218c1
dali012 <dali.jerbi97@gmail.com>
2024-10-25 15:55:39 +0200

    added vercel speedInsghits

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 import { Recursive } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
@@ -6,6 +5,7 @@ import Footer from "@/components/Footer";
 import { Toaster } from "@/components/ui/toaster";
 import Providers from "@/components/Providers";
 import { constructMetadata } from "@/lib/utils";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const recursive = Recursive({ subsets: ["latin"], display: "swap" });
 
@@ -22,7 +22,10 @@ export default function RootLayout({
         <Navbar />
         <main className="flex flex-col min-h-[calc(100vh-3.5rem-1px)] grainy-light">
           <div className="flex-1 flex flex-col h-full">
-            <Providers>{children}</Providers>
+            <Providers>
+              {children}
+              <SpeedInsights />
+            </Providers>
           </div>
           <Footer />
         </main>
