692c410a4b111a6c3f786c3bec1f4a31b54b5606
dali012 <dali.jerbi97@gmail.com>
2025-04-01 03:40:13 +0100

    fix hero section layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,18 +13,9 @@ export default function Home() {
     <div className="bg-slate-50">
       <ProductSchema />
       <section>
-        <MaxWidthWrapper className="pb-16 md:pb-24 pt-6 md:pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
-          <div className="col-span-2 px-4 md:px-6 lg:px-0 lg:pt-4">
+        <MaxWidthWrapper className="pb-16 md:pb-24 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pb-52">
+          <div className="col-span-2 px-4 md:px-6 lg:px-0">
             <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
-              <div className="absolute w-28 left-0 -top-20 hidden lg:block">
-                <Image
-                  src="/snake-1.webp"
-                  alt="CasedUp custom phone case design element"
-                  width={112} // Based on w-28
-                  height={112} // Approximated
-                  className="w-full"
-                />
-              </div>
               <h1
                 className="relative w-fit tracking-tight text-balance mt-10 md:mt-16 font-bold !leading-tight text-gray-900 text-4xl md:text-5xl lg:text-7xl"
                 style={{ textRendering: "optimizeSpeed" }}
